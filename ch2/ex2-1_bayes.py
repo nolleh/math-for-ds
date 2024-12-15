d2b80d1261390b7ebb00b7cdf492dfaf0e6de9c3
nolleh <nolleh7707@gmail.com>
2024-12-15 23:55:47 +0900

    chore: day #6

---
+++ ch2/ex2-1_bayes.py
@@ -0,0 +1,7 @@
+p_coffee_drinker = 0.65
+p_cancer = 0.005
+p_coffee_drinker_given_cancer = 0.85
+
+p_cancer_given_coffee_drinker = p_coffee_drinker_given_cancer * p_cancer / p_coffee_drinker
+
+print(p_cancer_given_coffee_drinker)
